fd3ee5948f7ae7a925d5e6974088ce088fadef62
jjcastillot <jjcastillot@gmail.com>
2022-07-27 21:55:40 -0500

    Corrected hovering function

--- js.js
+++ js.js
@@ -4,6 +4,14 @@ let gridSize = 16;
 
 for (let i=1;i<gridSize*gridSize+1;i++){
     let content = document.createElement('div');
-    content.classList.add(`C${i}`);
+    content.classList.add('blank');
     container.appendChild(content);      
 }
+
+function drawing(e) {
+    e.target.classList.add('draw');   
+}
+
+const sketch = Array.from(document.querySelectorAll('.blank'));
+sketch.forEach(square => square.addEventListener('mouseover', drawing));
+//sketch.addEventListener('mouseover', drawing);
