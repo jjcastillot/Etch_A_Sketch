34391ced51b7b6b5e6798d4a6f7a22dc9d78e2ee
jjcastillot <jjcastillot@gmail.com>
2022-08-01 22:00:54 -0500

    Added button to create grid

--- js.js
+++ js.js
@@ -1,23 +1,27 @@
 const container = document.querySelector('.container');
 gridSize = 0;
-do {
-    gridSize = prompt('Please introduce the grid size (Between 1-100)!');
-} while ((Number(gridSize)<1) || (Number(gridSize)>100));
 
+const createGridBtn = document.querySelector('.create_grid');
+createGridBtn.addEventListener('click', function(e) {
+    do {
+        gridSize = prompt('Please introduce the grid size (Between 1-100)!');
+    } while ((Number(gridSize)<1) || (Number(gridSize)>100));
+    
+    createGrid(container,gridSize);  
+    function drawing(e) {
+        e.target.classList.add('draw');   
+    }
+    const sketch = Array.from(document.querySelectorAll('.blank'));
+    sketch.forEach(square => square.addEventListener('mouseover', drawing));
+});
 
-container.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
-container.style['grid-template-rows'] = `repeat(${gridSize}, 1fr)`;
+function createGrid(container,gridSize) {
+    container.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
+    container.style['grid-template-rows'] = `repeat(${gridSize}, 1fr)`;
 
-for (let i=1;i<gridSize*gridSize+1;i++){
-    let content = document.createElement('div');
-    content.classList.add('blank');
-    container.appendChild(content);      
+    for (let i=1;i<gridSize*gridSize+1;i++){
+        let content = document.createElement('div');
+        content.classList.add('blank');
+        container.appendChild(content);      
+    }
 }
-
-function drawing(e) {
-    e.target.classList.add('draw');   
-}
-
-const sketch = Array.from(document.querySelectorAll('.blank'));
-sketch.forEach(square => square.addEventListener('mouseover', drawing));
-
