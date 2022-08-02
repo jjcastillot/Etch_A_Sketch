52f3eb3ee15eac110d36167b92008e8cc0f7e5fc
jjcastillot <jjcastillot@gmail.com>
2022-08-01 21:12:38 -0500

    Corrected the grid size and started with a prompt asking the user for it

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
-let gridSize = 16;
-
+let gridSize = prompt('Please introduce the grid size!');
+container.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
+container.style['grid-template-rows'] = `repeat(${gridSize}, 1fr)`;
 
 for (let i=1;i<gridSize*gridSize+1;i++){
     let content = document.createElement('div');
@@ -14,4 +15,4 @@ function drawing(e) {
 
 const sketch = Array.from(document.querySelectorAll('.blank'));
 sketch.forEach(square => square.addEventListener('mouseover', drawing));
-//sketch.addEventListener('mouseover', drawing);
+
